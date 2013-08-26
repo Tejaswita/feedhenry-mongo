ae5fb9587882e7df4a8ad9b98e085d71dbed2898
ttakawale@equalexperts.com <ttakawale@equalexperts.com>
2013-08-26 10:10:54 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -1,26 +1,11 @@
 $fh.ready(function() {
-
   $("#download_progress").html = "<p>upload a new file</p>";  
   $("#uploadFile").bind('click', function() {
     console.info('in upload');
     uploadToServer();   
-  });
-
- 
+  }); 
   $("#downloadFile").bind('click', function() {
     console.info('in download');
     downloadFromServer();
   });
- 
- var uploadFromClient = function() {
-    $fh.env({}, function(props) {
-    console.log("env properties: " + JSON.stringify(props));
-   });
- };
-
- var downloadFromServer = function() {
-    $fh.act({
-     act:'download'
-    });
- };
 });
