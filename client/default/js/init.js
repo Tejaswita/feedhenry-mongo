ee63d8c19ad9156dd1ed4da74bde589d8cbca7ed
ttakawale@equalexperts.com <ttakawale@equalexperts.com>
2013-08-26 05:06:29 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -2,7 +2,12 @@ $fh.ready(function() {
   document.getElementById("progress").innerHTML = "<p>upload a new file</p>";
   
   document.getElementById("uploadFile").onclick = function() {
-    
+    $fh.act({
+     act:'upload',
+     data: {
+       'filepath':
+     }
+    });
   }
  });
   
