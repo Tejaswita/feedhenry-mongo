d322b27c17fc569c5bcba16c1769d5023d66b1f0
ttakawale@equalexperts.com <ttakawale@equalexperts.com>
2013-08-26 10:12:46 +0000

    update file /client/default/js/fileops.js

--- client/default/js/fileops.js
+++ client/default/js/fileops.js
@@ -1,39 +1,40 @@
- var uploadToServer = function() {
-   console.log("in file upload");
-   $fh.file({
-     act:"upload",
-		 filepath:"init.js",
-		 server:"https://hpcs-gcvuuax99jryhs4ybcretsac-dev_hpcs.df.dev.u101.feedhenry.net/upload"
-	 }, function(response) {
-		 console.log("upload status " + response.res + ". Data being sent :" + response.size);
-	 }, function(msg, err){
-		 alert("Error" + msg); 
-	 });	 
- };
- var progressListener = function(progress) {
-      //progress value that is passed here is a float number between 0 and 1
-	    //convert it to percentage
-	    $('#download_progress').innerHTML('<p> current progress: ' + progress * 100 + '%' + '</p>');
-};
-var downloadFromServer = function() {
-  var fileSrc = "http://url";
-  var parts = fileSrc.split("/");
-	var destFile = parts[parts.length-1];	 
-	 $fh.file({
-		 act: 'download',
-		 src:  fileSrc,
-		 dest: destFile,
-		 progressListener:progressListener		 
-	 }, function(path) {
-		 $fh.webview({
-			 act:'open',
-			 url:path
-		 });
-	 }, function(msg, err) {
-		 alert(msg);
-	 })	;
- };
- 
+ var fileOps = { 
+      var uploadToServer = function() {
+       console.log("in file upload");
+       $fh.file({
+         act:"upload",
+    		 filepath:"init.js",
+    		 server:"https://hpcs-gcvuuax99jryhs4ybcretsac-dev_hpcs.df.dev.u101.feedhenry.net/upload"
+    	 }, function(response) {
+    		 console.log("upload status " + response.res + ". Data being sent :" + response.size);
+    	 }, function(msg, err){
+    		 alert("Error" + msg); 
+    	 });	 
+     };
+     var progressListener = function(progress) {
+          //progress value that is passed here is a float number between 0 and 1
+    	    //convert it to percentage
+    	    $('#download_progress').innerHTML('<p> current progress: ' + progress * 100 + '%' + '</p>');
+    };
+    var downloadFromServer = function() {
+      var fileSrc = "http://url";
+      var parts = fileSrc.split("/");
+    	var destFile = parts[parts.length-1];	 
+    	 $fh.file({
+    		 act: 'download',
+    		 src:  fileSrc,
+    		 dest: destFile,
+    		 progressListener:progressListener		 
+    	 }, function(path) {
+    		 $fh.webview({
+    			 act:'open',
+    			 url:path
+    		 });
+    	 }, function(msg, err) {
+    		 alert(msg);
+    	 })	;
+     };
+ }
 
 
 
