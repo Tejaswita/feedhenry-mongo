a09e9debb3b96e75e3072593d53274556a82c877
ttakawale@equalexperts.com <ttakawale@equalexperts.com>
2013-08-26 04:20:00 +0000

    update file /client/default/js/fileops.js

--- client/default/js/fileops.js
+++ client/default/js/fileops.js
@@ -14,7 +14,7 @@
 	    //convert it to percentage
 	    $('#download_progress').html('<p> current progress: ' + progress * 100 + '%' + '</p>');
 };
- function downloadFile() {
+var fDownload = function downloadFile() {
   var fileSrc = "http://url";
   var parts = fileSrc.split("/");
 	var destFile = parts[parts.length-1];
