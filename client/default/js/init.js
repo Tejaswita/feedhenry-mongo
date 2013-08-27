4b92aef159c3099b27ec9f81d541ea369dbf001d
Tejaswita <tejaswita@Tejaswita.(none)>
2013-08-27 10:44:01 +0530

    adding feedhenry dsk

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -1,4 +1,17 @@
-$fh.ready(function() {
+  var config = {
+        host: "https://hpcs.feedhenry.com/box/srv/1.1/pub/app/GcvuuewWzP61AXFW7_SmDy1z/refresh",
+        appid: "GcvuuaX99JRyhS4ybCRetsAC",
+        apikey: "ec2615c6e7da4e64f473da469cafcef6bb767a27",
+        mode: "dev"
+    };
+
+    $fh.init(config, function(res) {
+        alert("fh initialised");
+    }, function(err) {
+        alert("fh initialisation error");
+    });
+    
+ $fh.ready(function() {
     $("#download_progress").html = "<p>upload a new file</p>";  
     $("#uploadFile").bind('click', function() {
       console.info('in upload');
