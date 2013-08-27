10a8f766ea13174f3b64844422928ed9480b2eb1
ttakawale@equalexperts.com <ttakawale@equalexperts.com>
2013-08-27 05:46:46 +0000

    update file /client/default/js/videoops.js

--- client/default/js/videoops.js
+++ client/default/js/videoops.js
@@ -2,7 +2,6 @@
     var URL = win.URL || win.webkitURL,
         displayMessage = (function displayMessageInit() {
             var node = document.querySelector('#message');
-
             return function displayMessage(message, isError) {
                 node.innerHTML = message;
                 node.className = isError ? 'error' : 'info';
@@ -10,37 +9,25 @@
         }()),
         playSelectedFile = function playSelectedFileInit(event) {
             var file = this.files[0];
-
             var type = file.type;
-
             var videoNode = document.querySelector('video');
-
             var canPlay = videoNode.canPlayType(type);
-
             canPlay = (canPlay === '' ? 'no' : canPlay);
-
             var message = 'Can play type "' + type + '": ' + canPlay;
-
             var isError = canPlay === 'no';
-
             displayMessage(message, isError);
-
             if (isError) {
                 return;
             }
-
             var fileURL = URL.createObjectURL(file);
-
             videoNode.src = fileURL;
         },
-        inputNode = document.querySelector('input');
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    inputNode = document.querySelector('input');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
     if (!URL) {
         displayMessage('Your browser is not ' + 
            '<a href="http://caniuse.com/bloburls">supported</a>!', true);
         
         return;
-    }                
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    }
     inputNode.addEventListener('change', playSelectedFile, false);
 }(window));
