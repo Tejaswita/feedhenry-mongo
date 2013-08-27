86dc7b4029aaccd96aa033cfa29c6cd89994f437
ttakawale@equalexperts.com <ttakawale@equalexperts.com>
2013-08-27 05:53:07 +0000

    update file /client/default/js/init.js

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -1,15 +1,14 @@
-  var config = {
-        host: "https://hpcs.feedhenry.com/box/srv/1.1/pub/app/GcvuuewWzP61AXFW7_SmDy1z/refresh",
-        appid: "GcvuuaX99JRyhS4ybCRetsAC",
-        apikey: "ec2615c6e7da4e64f473da469cafcef6bb767a27",
-        mode: "dev"
-    };
-
-   // $fh.init(config, function(res) {
-   //     alert("fh initialised");
-   // }, function(err) {
-   //     alert("fh initialisation error");
-   // });
+var config = {
+  host: "https://hpcs.feedhenry.com",
+  appid: "GcvuuaX99JRyhS4ybCRetsAC",
+  appkey: "ec2615c6e7da4e64f473da469cafcef6bb767a27",
+  mode: "dev"
+};
+    $fh.init(config, function(res) {
+        alert("fh initialised");
+    }, function(err) {
+        alert("fh initialisation error");
+    });
     
  $fh.ready(function() {
     $("#download_progress").html = "<p>upload a new file</p>";  
